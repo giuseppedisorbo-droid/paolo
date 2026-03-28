d0a424704e4898585bcc616b569bdd69223c2b39
giuseppedisorbo-droid <giuseppedisorbo@gmail.com>
2026-03-28 13:24:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, doc, getDoc, setDoc, updateDoc, serverTimestamp, query, where, onSnapshot, orderBy, deleteDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-//const firebaseConfig = {apiKey: "AIzaSyB6CLQZHPG60LqsIKHAlS_Wt5OFXqfwqkw",authDomain: "antimo-6a86b.firebaseapp.com",projectId: "antimo-6a86b",storageBucket: "antimo-6a86b.firebasestorage.app",messagingSenderId: "671676764068",appId: "1:671676764068:web:95027e0babe3f30042fb31"};
 const firebaseConfig = {
-  apiKey: "AIzaSyAh7dwRs1j7Vxib7OlB7mVRB-MNthAo5NA",
+  apiKey: "AIzaSyAh7dwRs1j7Vxib70lB7mVRB-MNthAo5NA",
   authDomain: "peppe-ai-platform.firebaseapp.com",
   projectId: "peppe-ai-platform",
   storageBucket: "peppe-ai-platform.firebasestorage.app",
@@ -10,10 +9,18 @@ const firebaseConfig = {
   appId: "1:214462018633:web:f224407eba3b107e27fb98",
   measurementId: "G-54RQ3L1EDW"
 };
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 let currentUser = null;
-const cache = { ['location','Famiglie','Organizzazioni','people','external_workers'] {} };
+
+const Cache = {
+  location: {},
+  Famiglie: {},
+  Organizzazioni: {},
+  people: {},
+  external_workers: {}
+};
 let liveTasks = [], liveRequests = [], liveExpenses = [], liveCash = [], liveNotifications = [], liveLogs = [], liveWorkSessions = [];
 
 const ICONS = {
